8aad2c72b2b72e81f1f74d53b3cce834d83388f3
Ferriit <ferriitalt@gmail.com>
2026-07-08 12:28:47 +0200

    finished GLFW init

---
+++ src/engine/renderer.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <glm/glm.hpp>
+#include <GL/glext.h>
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+
+
+namespace vivianite {
+    class engine {
+        public:
+            int width = 800;
+            int height = 600;
+            
+            char* title = "Vivianite Window";
+
+            int FOV = 90;
+
+            bool init_status = true;
+
+            GLFWwindow* window;
+
+            static void error_callback(int error, const char* description) {
+                fprintf(stderr, "Error: %s\n", description);
+            }
+
+            engine() {
+                // GLFW
+                if (!glfwInit()) {
+                    this->init_status = false;
+                    return;
+                }
+                glfwSetErrorCallback(this->error_callback);
+            }
+
+            bool initialize() {
+                // OpenGL 3.3 CORE
+                glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+                glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+                glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+                // GLFW window
+                this->window = glfwCreateWindow(this->width, this->height, this->title, NULL, NULL);
+
+                if (!this->window) {
+                    return false;
+                }
+
+                glfwMakeContextCurrent(window);
+                
+                // GLEW init
+
+                return true;
+            }
+
+            ~engine() {
+                glfwDestroyWindow(window);
+                glfwTerminate();
+            }
+
+            bool check_status() {
+                return this->init_status;
+            }
+    };
+};
