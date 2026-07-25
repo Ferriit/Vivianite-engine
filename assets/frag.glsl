2f0d664ce96b574e93eb0630184c8d6da71811e4
Ferriit <ferriitalt@gmail.com>
2026-07-25 16:07:29 +0200

    fixed errors with gitignore and fixed normal issues

--- assets/frag.glsl
+++ assets/frag.glsl
@@ -1,10 +1,10 @@
 #version 460 core
 
-in vec3 vertexColor;
-in vec3 vertexNormal;
+in vec3 vertex_color;
+in vec3 vertex_cormal;
 
 out vec4 FragColor;
 
 void main() {
-    FragColor = vec4(vertexNormal, 1.0);
+    FragColor = vec4(vertex_ormal, 1.0);
 }
--- assets/vert.glsl
+++ assets/vert.glsl
@@ -4,15 +4,23 @@ layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
 layout(location = 2) in vec3 normal;
 
-out vec3 vertexColor;
-out vec3 vertexNormal;
+out vec3 frag_pos;
+out vec3 vertex_normal;
+out vec3 vertex_color;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main() {
-    gl_Position = projection * view * model * vec4(position, 1.0);
-    vertexColor = color;
-    vertexNormal = normal;
+void main()
+{
+    vec4 world_pos = model * vec4(position, 1.0);
+
+    frag_pos = world_pos.xyz;
+
+    vertex_normal = mat3(transpose(inverse(model))) * normal;
+
+    vertex_color = color;
+
+    gl_Position = projection * view * world_pos;
 }
