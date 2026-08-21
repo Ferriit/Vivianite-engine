4b6685fb75780d472a23c545b7850e51daf6631c
Ferriit <ferriit@proton.me>
2026-08-21 17:41:43 +0200

    fixed ntfs issues

--- cmake/mingw-w64.cmake
+++ cmake/mingw-w64.cmake
@@ -1,5 +1,15 @@
+# This file is only for Windows/MinGW builds.
+
 set(CMAKE_SYSTEM_NAME Windows)
 
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+if(MSYS OR MINGW)
+    # MSYS2 / MinGW environment
+    set(CMAKE_C_COMPILER gcc)
+    set(CMAKE_CXX_COMPILER g++)
+    set(CMAKE_RC_COMPILER windres)
+else()
+    # Linux cross-compilation
+    set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+    set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+    set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+endif()
